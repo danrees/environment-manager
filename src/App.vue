ed55ff00fa8f85cc208cc02e6d3ab1ecca3df28e
Daniel Rees <daniel.rees@autodata.net>
2017-11-06 16:54:21 -0500

    Fixes missing state issue (helps if you actually render something) and has a basic environment add function

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,35 @@
   <div id="app">
     <h1>Not default anymore</h1>
     <span>{{msg}}</span>
+    <div>{{newEnv}}</div>
+    <input placeholder="Put an environment name here" v-model="newEnv">
+    <button v-on:click="addEnv">Add Environment</button>
+    <ul>
+      <li v-for="env in environments" :key="env">{{ env }}</li>
+    </ul>
+    
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  props: ['msg']
+  props: ['msg'],
+  data() {
+    return {
+      newEnv: 'Starting'
+    }
+  },
+  methods: {
+    addEnv: function(event){
+      this.$store.commit('add',this.newEnv);
+    }
+  },
+  computed: {
+    environments() {
+      return this.$store.state.environments;
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 
 Vue.config.productionTip = false
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    environments: ["default environment"]
+  },
+  mutations: {
+    add(state,environment) {
+      state.environments.push(environment)
+    }
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store: store,
   template: '<App msg="Just starting" />',
   components: { App }
 })
